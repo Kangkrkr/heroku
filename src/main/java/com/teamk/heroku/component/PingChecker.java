017595d24a1387834ea6bb8b19bffabba2b6c5a7
teamk <kangkrkr@naver.com>
2016-11-18 06:57:33 +0900

    핑체커 추가

---
+++ src/main/java/com/teamk/heroku/component/PingChecker.java
@@ -0,0 +1,32 @@
+package com.teamk.heroku.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PingChecker {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+	
+	@Scheduled(fixedDelay = 1000 * 60 * 10)
+	public void sendPing(){
+		long start = System.currentTimeMillis();
+		ResponseEntity<String> status = restTemplate.getForEntity("https://my-socket-chat.herokuapp.com/", String.class);
+		long end = System.currentTimeMillis();
+		
+		if(status.getStatusCode().is2xxSuccessful())
+			System.err.println("Ping - OK , Loading time : " + ((end - start)) +"ms");
+		else
+			System.err.println(status.getStatusCodeValue());
+	}
+}
